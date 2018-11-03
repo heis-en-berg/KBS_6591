eccfca7720574d08e8c98d2ce3edbfb3605afa92
Karan Dhingra <karandhingra@Karans-MacBook-Air.local>
2018-11-03 01:36:38 -0400

    Added Expression and Fact structure

---
+++ src/problog/parser/Parser.java
@@ -0,0 +1,34 @@
+package problog.parser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import problog.model.EDB;
+import problog.model.Expression;
+
+public class Parser {
+
+//	public static void main(String[] args) 
+//	{ 
+//		EDB edb = new EDB();
+//		
+//		List<String> list1 = new ArrayList<String>();
+//		list1.add("a");
+//		list1.add("b");
+//		Expression exp1 = new Expression("p", list1, 0.2);
+//		
+//		List<String> list2 = new ArrayList<String>();
+//		list2.add("a");
+//		list2.add("b");
+//		Expression exp2 = new Expression("p", list2, 0.3);
+//		
+//		edb.addFact(exp1);
+//		edb.addFact(exp2);
+//		HashMap<List<String>, Double > set = edb.facts.get("p");
+//		System.out.println(set);
+//	} 
+	
+}
