2cf075251440d3dc938f931f6c54a57578b8c4a0
sahil sodhi <sahilsodhi@sahils-MacBook-Pro.local>
2018-11-08 13:01:42 -0500

    Parser added functionality for addRule and andFacts.

--- src/problog/parser/Parser.java
+++ src/problog/parser/Parser.java
@@ -15,9 +15,79 @@ public class Parser {
         db = new DB();
         readFile();
     }
+
+    /* Method to add rules to DB. */
+    public void  addRule(String rule){
+        String[] splitRule = rule.split(":-");
+
+        /* Retrieve rule predicate name.*/
+        String[] rulePredicate = splitRule[0].split("\\(");
+        String predicate = rulePredicate[0];
+
+        /* Retrieve rule probability. */
+        int index = rule.lastIndexOf(":");
+        String probability = rule.substring(index+1,rule.length()-1);
+        Double prob = Double.parseDouble(probability);
+
+        /* Retrieve rule head terms*/
+        rulePredicate[1] = rulePredicate[1].substring(0,rulePredicate[1].length()-1);
+        List<String> listOfTerms = new ArrayList<>();
+        String[] addAtoms = rulePredicate[1].split(",");
+        for (String y : addAtoms) {
+            listOfTerms.add(y);
+        }
+        Expression headExpression = new Expression(predicate,listOfTerms,prob);
+
+        /* Expression for rule body */
+        String body = splitRule[1].substring(0,splitRule[1].lastIndexOf(":"));
+        body = body.concat(",");
+        String[] bodyExpressions = body.split("\\),");
+        ArrayList<Expression> bodyPredicates = new ArrayList<>();
+        for (String y : bodyExpressions) {
+            List<String> listOfBodyTerms = new ArrayList<>();
+            /* Body Predicates. */
+            String[] ruleBodyPredicate = y.split("\\(");
+            String bodyPredicate = ruleBodyPredicate[0];
+            String[] bodyTerms = ruleBodyPredicate[1].split(",");
+            for (String z : bodyTerms) {
+                listOfBodyTerms.add(z);
+            }
+            Expression bodyExpression = new Expression(bodyPredicate,listOfBodyTerms,null);
+            bodyPredicates.add(bodyExpression);
+        }
+        db.idb.addRule(headExpression,bodyPredicates);
+    }
+
+    /* Method to add facts to DB*/
+    public  void addFact(String fact){
+        /* Retrieve fact predicate name. */
+        String[] splitFact = fact.split("\\(");
+        String predicate = splitFact[0];
+
+        /* Retrieve fact probability. */
+        String[] splitFactProb = fact.split(":");
+        String probability = splitFactProb[1];
+        probability = probability.substring(0,probability.length()-1);
+        Double prob = Double.parseDouble(probability);
+
+        /* Retrieve fact terms as a List. */
+        String atoms = splitFact[1];
+        String[] splitAtoms = atoms.split("\\)");
+        List<String> listOfTerms = new ArrayList<>();
+        atoms = splitAtoms[0];
+        String[] addAtoms = atoms.split(",");
+        for (String y : addAtoms) {
+            listOfTerms.add(y);
+            System.out.print(y);
+        }
+        /* Add the given fact in db. */
+        Expression expression = new Expression(predicate, listOfTerms, prob);
+        db.edb.addFact(expression);
+    }
+
     public void readFile() {
 
-        /* Read Facts from file.*/
+        /* Read clauses from file. */
         Scanner consoleScanner = new Scanner(System.in);
         System.out.print("Enter file path: ");
         String filePath = consoleScanner.nextLine();
@@ -26,34 +96,23 @@ public class Parser {
 
         try{
             Scanner lineScanner = new Scanner(file);
-            while(lineScanner.hasNextLine()){
-                String fact = lineScanner.nextLine();
+            while(lineScanner.hasNextLine()) {
+                String clause = lineScanner.nextLine();
 
                 /* Remove unnecessary whitespace. */
-                fact = fact.replaceAll(" ","");
+                clause = clause.replaceAll(" ", "");
 
-                /* Retrieve fact predicate name. */
-                String[] splitFact = fact.split("\\(");
-                String predicate = splitFact[0];
+                /* Differentiate between a rule and a fact.*/
+                CharSequence test = ":-";
+                boolean test_result = clause.contains(test);
 
-                /* Retrieve fact probability. */
-                String[] splitFactProb = fact.split(":");
-                String probability = splitFactProb[1];
-                Double prob = Double.parseDouble(probability);
-
-                /* Retrieve fact terms as a List. */
-                String atoms = splitFact[1];
-                String[] splitAtoms = atoms.split("\\)");
-                List<String> listOfTerms = new ArrayList<>();
-                atoms = splitAtoms[0];
-                String[] addAtoms = atoms.split(",");
-                for(String y: addAtoms){
-                    listOfTerms.add(y);
+                /*Add Rule*/
+                if (test_result) {
+                    addRule(clause);
                 }
-
-                /* Add the given fact in db. */
-                Expression expression = new Expression(predicate,listOfTerms,prob);
-                db.edb.addFact(expression);
+                else {
+                    addFact(clause);
+                }
             }
             lineScanner.close();
         }
