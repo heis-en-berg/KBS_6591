dde49f09cfa315d41a89e9cab4c54e79bb7af315
sahil sodhi <sahilsodhi@sahils-mbp.wireless.concordia.ca>
2018-11-22 19:35:31 -0500

    Semi Naive Evaluation. Code to create Extra EDBa.

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,15 +2,22 @@ package main;
 
 import problog.evaluation.NaiveEvaluator;
 import problog.parser.Parser;
+import problog.evaluation.SemiNaiveEvaluator;
 
 public class Main {
 
     public  static  void main(String[] args){
 
         Parser parser = new Parser();
-        NaiveEvaluator naiveEvaluator = new NaiveEvaluator();
-        naiveEvaluator.performNaiveEvaluation(parser.db);
+
+//        NaiveEvaluator naiveEvaluator = new NaiveEvaluator();
+//        naiveEvaluator.performNaiveEvaluation(parser.db);
+
+        SemiNaiveEvaluator seminaiveEvaluator = new SemiNaiveEvaluator();
+        seminaiveEvaluator.performSemiNaiveEvaluation(parser.db, parser.extraEDB);
 
     }
 
 }
+
+// /Users/sahilsodhi/Documents/COMP6591/Sample.txt
---
+++ src/problog/model/ExtraEDB.java
@@ -0,0 +1,9 @@
+package problog.model;
+
+public class ExtraEDB {
+    public  EDB EDBObject[];
+    public ExtraEDB(int numberOfRules){
+
+        EDBObject = new EDB[numberOfRules];
+    }
+}
