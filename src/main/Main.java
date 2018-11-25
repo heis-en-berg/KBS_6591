6a1e19cab07a9a03c5623f73e34477e54e8eb86b
sahil sodhi <sahilsodhi@sahils-MacBook-Pro.local>
2018-11-25 15:43:32 -0500

    Extra edb cleanup.

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,15 +14,6 @@ public class Main {
 //
         SemiNaiveEvaluator seminaiveEvaluator = new SemiNaiveEvaluator();
         seminaiveEvaluator.performSemiNaiveEvaluation(parser.db);
-//        List<String> listex = new ArrayList<>();
-//        listex.add("Sodhi");
-//        String sahil = "Sahil";
-//        Double val = 0.5;
-//        HashMap<String,HashMap<List<String>,Double>> fact = new HashMap<>();
-//        HashMap<List<String>,Double> factList = new HashMap<>();
-//        factList.put(listex,val);
-//        fact.put(sahil,factList);
-//        System.out.println("factList keyset value:" +factList.get(listex));
     }
 }
 
